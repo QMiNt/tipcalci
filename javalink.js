17b0751453c80c1dd70e3eedb5f5a85c6b3c6714
Aditya Agarwal <agarwaladitya7257@outlook.com>
2022-04-29 17:28:58 +0530

    initialCommit

---
+++ javalink.js
@@ -0,0 +1,14 @@
+const { parse } = require("postcss");
+
+function calc()
+{
+    let a=parseInt(document.getElementById("bill_i").value);
+    let b=parseInt(document.getElementById("tipi").value);
+    let c=parseInt(document.getElementById("nopi").value);
+    let d=(b*a)/100;
+    let e=d+a;
+    document.getElementById("otip").value=(b*a)/100;
+    document.getElementById("otpp").value=d/c;
+    document.getElementById("totbil").value= e;
+}
+
